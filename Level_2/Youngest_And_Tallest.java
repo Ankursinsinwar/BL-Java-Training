d69a899703b0546ffd8175b12ffd22320ae9bfe0
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2026-02-07 15:21:58 +0530

    Add: Youngest and tallest amoung three friends problem for Arrays

---
+++ Level_2/Youngest_And_Tallest.java
@@ -0,0 +1,29 @@
+package Level_2;
+
+import java.util.Scanner;
+
+public class Youngest_And_Tallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] friends = {"Amar", "Akbar", "Anthony"};
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + friends[i] + ": ");
+            age[i] = sc.nextInt();
+            System.out.print("Enter height of " + friends[i] + ": ");
+            height[i] = sc.nextDouble();
+        }
+
+        int youngestIndex = 0, tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[youngestIndex]) youngestIndex = i;
+            if (height[i] > height[tallestIndex]) tallestIndex = i;
+        }
+
+        System.out.println("Youngest: " + friends[youngestIndex]);
+        System.out.println("Tallest: " + friends[tallestIndex]);
+        sc.close();
+    }
+}
